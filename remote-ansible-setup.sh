dec0a90a08e33bb418eb3d78456c3e8b83ae9588
Greg Kulasik <gkulasik@gmail.com>
2021-04-08 09:48:15 -0500

    Set +x on setup script and update installed items

--- remote-ansible-setup.sh
+++ remote-ansible-setup.sh
@@ -50,28 +50,6 @@ update() {
     [ $? != 0 ] && { logThis "apt-get update had an error.  Stopping now!"; exit 1; } || { logThis "apt-get update completed successfully."; }
 }
 
-sshServer() {
-	sshCheck=$(netstat -natp | grep [s]shd | grep LISTEN | grep -v tcp6)
-	[ $? != 0 ] && { logThis "openssh-server is NOT installed."; installOpenssh; } || { logThis "openssh-server is running."; }
-}
-
-installOpenssh() {
-	logThis "Installing openssh-server."
-	installSSH=$(sudo /usr/bin/apt-get install openssh-server -qy)
-	[ $? != 0 ] && { logThis "apt-get install openssh-server had an error.  Stopping now!"; exit 1; } || { logThis "apt-get install openssh-server completed successfully."; }
-}
-
-curlCLI() {
-	curlCheck=$(which curl)
-	[ $? != 0 ] && { logThis "curl is NOT installed."; installCurl; } || { logThis "curl is available."; }
-}
-
-installCurl() {
-	logThis "Installing curl."
-	installSSH=$(sudo /usr/bin/apt-get install curl -qy)
-	[ $? != 0 ] && { logThis "apt-get install curl had an error.  Stopping now!"; exit 1; } || { logThis "apt-get install curl completed successfully."; }
-}
-
 python3() {
 	curlCheck=$(which python3)
 	[ $? != 0 ] && { logThis "python3 is NOT installed."; installPython3; } || { logThis "python is available."; }
@@ -108,10 +86,11 @@ installAvahiDaemon() {
 verifyRoot
 init
 update
-sshServer
-curlCLI
 python3
 sshpass
 avahiDaemon
+
+logThis "================="
+logThis "Ansible setup complete - if no errors reported this server is ready for Ansible administration."
 
 exit 0
